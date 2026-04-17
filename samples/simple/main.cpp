32abab0fc45cb46eb453a4b5306ddbf79a2296ff
kassa_ <pierre.jacobs852@gmail.com>
2026-04-17 19:06:00 +0200

    #1 Remove task.h

--- samples/simple/main.cpp
+++ samples/simple/main.cpp
@@ -3,31 +3,17 @@
 #include <gqlxy/links/http_link.h>
 #include <gqlxy/links/split_link.h>
 #include <gqlxy/links/ws_link.h>
-#include <gqlxy/task.h>
 
 #include <iostream>
 
 using namespace std;
 using namespace gqlxy;
 
-Task<int> run(Client& client) {
-    auto result = co_await client.Query({.query = R"( query { __typename } )"});
-
-    if (result.data)
-        cout << result.data->dump(2) << endl;
-    else if (result.errors)
-        for (const auto& e : *result.errors) cerr << "Error: " << e.message << endl;
-
-    co_return 0;
-}
-
 int main() {
     // clang-format off
     Client client({
         .link = make_shared<SplitLink>(
-            [](const GraphQLRequest& req) {
-                return req.query.find("subscription") == string::npos;
-            },
+            [](const auto& req) { return req.type != OperationType::Subscription; },
             make_shared<HttpLink>(HttpLinkOptions{.url = "http://localhost:4000/graphql"}),
             make_shared<WsLink>(WsLinkOptions{.url = "ws://localhost:4000/graphql"})
         ),
@@ -35,19 +21,40 @@ int main() {
     });
     // clang-format on
 
-    run(client).get();
-
-    client.Query({.query = R"( query { __typename } )"})
+    client.Query({
+        .query = R"(
+            query {
+                __typename
+            }
+        )"
+    })
         .subscribe(
-            [](const GraphQLResponse& r) { cout << r.data->dump(2) << endl; },
-            [](exception_ptr) { cerr << "Query error" << endl; }
+            [](const auto& r) {
+                auto [data, errors] = r;
+
+                if (data) cout << r.data->dump(2) << endl;
+                if (errors)
+                    for (const auto& [message, _] : *r.errors)
+                        cerr << "Error: " << message << endl;
+            },
+            [](auto) { cerr << "Query error" << endl; }
         );
 
-    auto sub = client.Subscribe({.query = R"( subscription { onMessage { text } } )"})
+    auto sub = client.Subscribe({
+        .query = R"(
+            subscription {
+                onMessage {
+                    text
+                }
+            }
+        )"})
         .subscribe(
-            [](const GraphQLResponse& r) { cout << r.data->dump(2) << endl; },
-            [](exception_ptr) { cerr << "Subscription error" << endl; }
+            [](const auto& r) { cout << r.data->dump(2) << endl; },
+            [](auto) { cerr << "Subscription error" << endl; }
         );
+
+    string input;
+    getline(cin, input);
 
     return 0;
 }
